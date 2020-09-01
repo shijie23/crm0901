eb59cba6f3ad1b8380de1651cc14d5bad7b3d2b3
shijie <45888162+shijie23@users.noreply.github.com>
2020-09-02 07:46:38 +0800

    Update Test01.java

--- src/com/hsj/github/test/Test01.java
+++ src/com/hsj/github/test/Test01.java
@@ -5,6 +5,7 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("Hello github");
         System.out.println("Hello Local");
+        System.out.println("Hello Local 02");
     }
 
 }
